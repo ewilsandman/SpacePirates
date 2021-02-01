11f520d5d401c27106d7c697848188386b6071ba
ewilsandman <56822177+ewilsandman@users.noreply.github.com>
2021-02-01 11:30:57 +0100

    Ai1

--- Assets/Scripts/Cnavas scripts/LevelManeger.cs
+++ Assets/Scripts/Cnavas scripts/LevelManeger.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class LevelManeger : MonoBehaviour
 {
+    void Start()
+    {
+        if ("GameOverscean" == SceneManager.GetActiveScene().name)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        if( "victory" == SceneManager.GetActiveScene().name)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+    }
     public void StartGame()
     {
         SceneManager.LoadScene("2_LVL");

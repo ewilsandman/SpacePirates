65482a0d3d6469ac80359950d567cbfa927f1410
Erik-sengkey <56818074+Erik-sengkey@users.noreply.github.com>
2021-02-11 11:58:57 +0100

    begin target projectiel

---
+++ Assets/Scripts/Gunscripts/GuidedMisseil.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GuidedMisseil : MonoBehaviour
+{
+    [SerializeField]
+    private float acceleration = 20f;
+
+    [SerializeField]
+    private float accelerationTime = 7f;
+
+    [SerializeField]
+    private float missileSpeed = 20f;
+
+    [SerializeField]
+    private float turnRate = 50f;
+
+    [SerializeField]
+    private Transform target;
+
+    private bool missileActive = false;
+
+    private bool isAccelareting = false;
+
+    private float accelerationActiveTime = 0f;
+
+    private Quaternion guideRotation;
+
+    private bool targetTracking = false;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        ActivetMissile();
+    }
+
+    private void ActivetMissile()
+    {
+        missileActive = true;
+        targetTracking = true;
+        accelerationActiveTime = Time.time;
+    }
+
+    private void Update()
+    {
+        Run();
+        GuidedMissile();
+    }
+
+    private void Run()
+    {
+        if (Since(accelerationActiveTime) > accelerationTime)
+            isAccelareting = false;
+        else
+            isAccelareting = true;
+
+        if (!missileActive) return;
+
+        if (isAccelareting)
+            missileSpeed += acceleration * Time.time;
+        
+        rb.velocity = transform.forward * missileSpeed;
+
+        if (targetTracking)
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, guideRotation, turnRate * Time.deltaTime);
+
+        Debug.Log("Missile speed" + missileSpeed);
+    }
+
+    private float Since(float since)
+    {
+        return Time.time - since;
+    }
+
+    private void GuidedMissile()
+    {
+        if (target == null) return;
+
+        if(targetTracking)
+        {
+            Vector3 relativePosition = target.position - transform.position;
+            guideRotation = Quaternion.LookRotation(relativePosition, transform.up);
+        }
+
+        Debug.Log("Guiding");
+    }
+}
---
+++ Assets/Scripts/Gunscripts/launcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class launcher : MonoBehaviour
+{
+    public float firerate;
+
+    private bool fire;
+
+    private float nextlanch;
+
+    private void Update()
+    {
+        fire = Input.GetKey(KeyCode.Space);
+
+        if(fire && Time.time >= nextlanch)
+        {
+            nextlanch = Time.time + firerate;
+            FireMissile();
+        }
+    }
+
+    private void FireMissile()
+    {
+        Debug.Log("Fire");
+    }
+}

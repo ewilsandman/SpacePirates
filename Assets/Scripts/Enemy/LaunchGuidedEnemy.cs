32674f09117cf0867a2e3c892906068ff9f38d3f
Erik-sengkey <56818074+Erik-sengkey@users.noreply.github.com>
2021-02-26 12:14:52 +0100

    yhing

---
+++ Assets/Scripts/Enemy/LaunchGuidedEnemy.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaunchGuidedEnemy : MonoBehaviour
+{
+    [SerializeField]
+    private float firerate;
+
+    [SerializeField]
+    private float nextlanch;
+
+    [SerializeField]
+    private GuidedMisseil missile;
+
+    [SerializeField]
+    private Transform target;
+
+    [SerializeField]
+    private Transform launchSpot;
+
+    private void Update()
+    {
+        if (Time.time >= nextlanch)
+        {
+            nextlanch = Time.time + firerate;
+            FireMissile();
+        }
+    }
+
+    private void FireMissile()
+    {
+        GuidedMisseil newMisseil = Instantiate(missile, launchSpot.position, launchSpot.rotation);
+        newMisseil.target = target;
+        Debug.Log("Fire");
+    }
+}
